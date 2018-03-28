2d4a7aa5eb57e3b33ea5cfa93de3622d529d537b
artinmajdi <msm2024@gmail.com>
2018-03-28 12:16:07 -0700

    fd

--- SavingSlicesTiff_V5_SingleClass_2DSlicesSeperately.py
+++ SavingSlicesTiff_V5_SingleClass_2DSlicesSeperately.py
@@ -8,10 +8,11 @@ import tifffile
 import pickle
 from PIL import ImageEnhance , Image , ImageFilter
 
-NeucleusFolder = 'CNN_VLP4_2DSlicesSeperately'
-NeuclusName = '6-VLP'
+NeucleusFolder = 'CNN_VLP5_2DSlicesSeperately_Thalamus'
+NeuclusName = '1-THALAMUS' # '6-VLP'
 
-Directory_Priors = '/media/data1/artin/data/Thalamus/'+ NeucleusFolder + '/OriginalDeformedPriors'
+# Directory_Priors = '/media/data1/artin/data/Thalamus/'+ NeucleusFolder + '/OriginalDeformedPriors'
+Directory_Priors = '/media/data1/artin/data/Thalamus/OriginalDeformedPriors'
 SegmentName = 'ManualDelineation/' + NeuclusName + '_Deformed.nii.gz'   # ThalamusSegDeformed  ThalamusSegDeformed_Croped    PulNeucleusSegDeformed  PulNeucleusSegDeformed_Croped
 
 subFolders = os.listdir(Directory_Priors)
@@ -35,6 +36,7 @@ SliceNumbers = range(107,140)
 
 
 
+
 for ii in range(len(A)):
     if ii == 0:
         TestName = 'WMnMPRAGE_bias_corr_Deformed' # _Deformed_Cropped
@@ -46,62 +48,62 @@ for ii in range(len(A)):
     inputName = TestName + '.nii.gz'
 
 
-    for sFi in range(len(subFolders)-16):
-        # sFi = 0
+    # for sFi in range(len(subFolders)-16):
+    sFi = 0
 
-        mask   = nib.load(Directory_Priors + '/'  + subFolders2[sFi] + '/' + SegmentName)
-        im     = nib.load(Directory_Priors + '/'  + subFolders2[sFi] + '/' + inputName)
-        print im.shape
-        print subFolders2[sFi]
-        imD    = im.get_data()
-        maskD  = mask.get_data()
-        Header = im.header
-        Affine = im.affine
+    mask   = nib.load(Directory_Priors + '/'  + subFolders2[sFi] + '/' + SegmentName)
+    im     = nib.load(Directory_Priors + '/'  + subFolders2[sFi] + '/' + inputName)
+    print im.shape
+    print subFolders2[sFi]
+    imD    = im.get_data()
+    maskD  = mask.get_data()
+    Header = im.header
+    Affine = im.affine
 
-        # imD2 = imD
-        # maskD2 = maskD
+    # imD2 = imD
+    # maskD2 = maskD
 
-        imD2 = imD[50:198,130:278,SliceNumbers]
-        maskD2 = maskD[50:198,130:278,SliceNumbers]
+    imD2 = imD[50:198,130:278,SliceNumbers]
+    maskD2 = maskD[50:198,130:278,SliceNumbers]
 
-        padSizeFull = 90
-        padSize = padSizeFull/2
-        imD_padded = np.pad(imD2,((padSize,padSize),(padSize,padSize),(0,0)),'constant' ) #
-        maskD_padded = np.pad(maskD2,((padSize,padSize),(padSize,padSize),(0,0)),'constant' ) # , constant_values=(5)
+    padSizeFull = 90
+    padSize = padSizeFull/2
+    imD_padded = np.pad(imD2,((padSize,padSize),(padSize,padSize),(0,0)),'constant' ) #
+    maskD_padded = np.pad(maskD2,((padSize,padSize),(padSize,padSize),(0,0)),'constant' ) # , constant_values=(5)
 
-        # imD2 = imD[IndxX,:,:]
-        # imD2 = imD2[:,IndxY,:]
-        # imD2 = imD2[:,:,IndxZ]
-        #
-        # maskD2 = maskD[IndxX,:,:]
-        # maskD2 = maskD2[:,IndxY,:]
-        # maskD2 = maskD2[:,:,IndxZ]
+    # imD2 = imD[IndxX,:,:]
+    # imD2 = imD2[:,IndxY,:]
+    # imD2 = imD2[:,:,IndxZ]
+    #
+    # maskD2 = maskD[IndxX,:,:]
+    # maskD2 = maskD2[:,IndxY,:]
+    # maskD2 = maskD2[:,:,IndxZ]
 
-        for p in range(len(subFolders)):
+    for p in range(len(subFolders)):
 
-            if sFi == p:
-                SaveDirectoryImage = Directory_Test + '/' + subFolders2[sFi] + '/' + '/Test'
-            else:
-                SaveDirectoryImage = Directory_Test + '/' + subFolders2[sFi] + '/' + '/Train'
+        if sFi == p:
+            SaveDirectoryImage = Directory_Test + '/' + subFolders2[sFi] + '/' + '/Test'
+        else:
+            SaveDirectoryImage = Directory_Test + '/' + subFolders2[sFi] + '/' + '/Train'
+
+        try:
+            os.stat(SaveDirectoryImage)
+        except:
+            os.makedirs(SaveDirectoryImage)
+
+        for sliceInd in range(imD2.shape[2]):
+
+            SaveDirectoryImageSlice = SaveDirectoryImage + '/Slice' + str(sliceInd)
 
             try:
-                os.stat(SaveDirectoryImage)
+                os.stat(SaveDirectoryImageSlice)
             except:
-                os.makedirs(SaveDirectoryImage)
+                os.makedirs(SaveDirectoryImageSlice)
 
-            for sliceInd in range(imD2.shape[2]):
-
-                SaveDirectoryImageSlice = SaveDirectoryImage + '/Slice' + str(sliceInd)
-
-                try:
-                    os.stat(SaveDirectoryImageSlice)
-                except:
-                    os.makedirs(SaveDirectoryImageSlice)
-
-                if sFi == p:
-                    tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(sliceInd)+'.tif',imD_padded[:,:,sliceInd])
-                    tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(sliceInd)+'_mask.tif',maskD_padded[:,:,sliceInd])
-                else:
-                    for i in range(max(0,sliceInd-1),min(sliceInd+2,imD2.shape[2])):
-                        tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(i)+'.tif',imD_padded[:,:,i])
-                        tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(i)+'_mask.tif',maskD_padded[:,:,i])
+            if sFi == p:
+                tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(sliceInd)+'.tif',imD_padded[:,:,sliceInd])
+                tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(sliceInd)+'_mask.tif',maskD_padded[:,:,sliceInd])
+            else:
+                for i in range(max(0,sliceInd-1),min(sliceInd+2,imD2.shape[2])):
+                    tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(i)+'.tif',imD_padded[:,:,i])
+                    tifffile.imsave(SaveDirectoryImageSlice + '/' + subFolders2[p] + '_Slice'+str(i)+'_mask.tif',maskD_padded[:,:,i])
--- main_V4.py
+++ main_V4.py
@@ -7,7 +7,8 @@ import nibabel as nib
 import shutil
 from collections import OrderedDict
 import logging
-from TestData import TestData
+from TestData_V4 import TestData
+from tf_unet import unet, util, image_util
 
 
 def DiceCoefficientCalculator(msk1,msk2):
@@ -33,100 +34,106 @@ def ReadMasks(DirectoryMask,SliceNumbers):
 def SumMasks(DirectorySubFolders):
 
     i = 1
-    SegmentName = '/6_VLP_NeucleusSegDeformed.nii.gz'
-    msk , Header , Affine = ReadMasks(DirectorySubFolders+SegmentName)
+    Directory_Nuclei_Label = '/6_VLP_NeucleusSegDeformed.nii.gz'
+    msk , Header , Affine = ReadMasks(DirectorySubFolders+Directory_Nuclei_Label)
     maskD = np.zeros(msk.shape)
     maskD[msk == 1] = 10*i
 
     i = i + 1
-    SegmentName = '/7_VPL_NeucleusSegDeformed.nii.gz'
-    msk , Header , Affine = ReadMasks(DirectorySubFolders+SegmentName)
+    Directory_Nuclei_Label = '/7_VPL_NeucleusSegDeformed.nii.gz'
+    msk , Header , Affine = ReadMasks(DirectorySubFolders+Directory_Nuclei_Label)
     maskD[msk == 1] = 10*i
 
     i = i + 1
-    SegmentName = '/8_Pul_NeucleusSegDeformed.nii.gz'
-    msk , Header , Affine = ReadMasks(DirectorySubFolders+SegmentName)
+    Directory_Nuclei_Label = '/8_Pul_NeucleusSegDeformed.nii.gz'
+    msk , Header , Affine = ReadMasks(DirectorySubFolders+Directory_Nuclei_Label)
     maskD[msk == 1] = 10*i
 
     i = i + 1
-    SegmentName = '/12_MD_Pf_NeucleusSegDeformed.nii.gz'
-    msk , Header , Affine = ReadMasks(DirectorySubFolders+SegmentName)
+    Directory_Nuclei_Label = '/12_MD_Pf_NeucleusSegDeformed.nii.gz'
+    msk , Header , Affine = ReadMasks(DirectorySubFolders+Directory_Nuclei_Label)
     maskD[msk == 1] = 10*i
 
     return maskD , Header , Affine
 
-NeucleusFolder = 'CNN_VLP4_2DSlicesSeperately' #'CNN_Thalamus' #
-NucleusName = '6-VLP' #'1-THALAMUS' #
+NeucleusFolder = 'CNN5_Thalamus_2DSlicesSeperately_' #'CNN_Thalamus' #
+NucleusName = '1-THALAMUS' #'6-VLP' #
 
 A = [[0,0],[4,3]] # ,[6,1],[1,2],[1,3],[4,1]
 SliceNumbers = range(107,140)
 
-Directory_Nuclei = '/media/data1/artin/data/Thalamus/' + NeucleusFolder
-Directory_Thalamus = '/media/data1/artin/data/Thalamus/' + 'CNN_Thalamus'
+Directory_main = '/media/data1/artin/data/Thalamus/'
+Directory_Nuclei_Full = Directory_main + NeucleusFolder
+Directory_Thalamus_Full = Directory_main + 'CNN_Thalamus'
 
 
 for ii in range(len(A)):
+# ii = 0
     if ii == 0:
         TestName = 'Test_WMnMPRAGE_bias_corr_Deformed' # _Deformed_Cropped
     else:
         TestName = 'Test_WMnMPRAGE_bias_corr_Sharpness_' + str(A[ii][0]) + '_Contrast_' + str(A[ii][1]) + '_Deformed'
 
-    Test_Directory_Nuclei = Directory_Nuclei + '/' + TestName + '/'
-
-    # with open(Directory_Nuclei + '/OriginalDeformedPriors/subFolderList.txt' ,"rb") as fp:
+    Directory_Nuclei = Directory_Nuclei_Full + '/' + TestName + '/'
+    print Directory_Nuclei
+    # with open(Directory_Nuclei_Full + '/OriginalDeformedPriors/subFolderList.txt' ,"rb") as fp:
     #     subFolders = pickle.load(fp)
-    subFolders = os.listdir(Test_Directory_Nuclei)
-    print len(subFolders)
+    subFolders = os.listdir(Directory_Nuclei)
+    # print len(subFolders)
 
 
     for sFi in range(len(subFolders)):
-        print('Test: ' + str(A[ii]) + 'Subject: ' + str(subFolders[sFi]))
-    # sFi = 1
 
-        Segments_Directory = Directory_Nuclei + '/OriginalDeformedPriors/' +  subFolders[sFi] + '/ManualDelineation'
-        SegmentName = Segments_Directory +'/' + NucleusName + '_Deformed.nii.gz'   # ThalamusSegDeformed  ThalamusSegDeformed_Croped    PulNeucleusSegDeformed  PulNeucleusSegDeformed_Croped
+        Directory_Nuclei_Label = Directory_main + '/OriginalDeformedPriors/' +  subFolders[sFi] + '/ManualDelineation/' + NucleusName + '_Deformed.nii.gz'   # ThalamusSegDeformed  ThalamusSegDeformed_Croped    PulNeucleusSegDeformed  PulNeucleusSegDeformed_Croped
 
+        for sliceInd in range(33):
+            print('------------------------------------------------------')
+            print('Test: ' + str(A[ii]) + ' Subject: ' + str(subFolders[sFi]) + ' Slide ' + str(sliceInd) )
+            print('------------------------------------------------------')
 
-        for sliceInd in range(32):
-
-            Test_Path_Nuclei  = Test_Directory_Nuclei + subFolders[sFi] + '/Test/Slice' + str(sliceInd) + '/'
-            Train_Path_Nuclei = Test_Directory_Nuclei + subFolders[sFi] + '/Train/Slice' + str(sliceInd) + '/'
-            Trained_Model_Path_Nuclei = Train_Path_Nuclei + 'model/'
-            TestResults_Path_Nuclei   = Test_Path_Nuclei  + 'results/'
-
-            try:
-                os.stat(Trained_Model_Path_Nuclei)
-            except:
-                os.makedirs(Trained_Model_Path_Nuclei)
+            # sliceInd = 25
+            Directory_Nuclei_Test  = Directory_Nuclei + subFolders[sFi] + '/Test/Slice' + str(sliceInd) + '/'
+            Directory_Nuclei_Train = Directory_Nuclei + subFolders[sFi] + '/Train/Slice' + str(sliceInd) + '/'
+            Directory_Nuclei_Train_Model = Directory_Nuclei_Train + 'model/'
 
             try:
-                os.stat(TestResults_Path_Nuclei)
+                os.stat(Directory_Nuclei_Train_Model)
             except:
-                os.makedirs(TestResults_Path_Nuclei)
+                os.makedirs(Directory_Nuclei_Train_Model)
+
+            try:
+                os.stat(Directory_Nuclei_Test  + 'results/')
+            except:
+                os.makedirs(Directory_Nuclei_Test  + 'results/')
 
 
-            TrainData = image_util.ImageDataProvider(Train_Path_Nuclei + "*.tif")
-            print TrainData.n_class
+            TrainData = image_util.ImageDataProvider(Directory_Nuclei_Train + "*.tif")
+
+            # print Directory_Nuclei_Test
+            # TestData = image_util.ImageDataProvider(  Directory_Nuclei_Test + '*.tif',shuffle_data=False)
+            # print len(TrainData.data_files)
+            # data , label = TrainData(len(TrainData.data_files))
+
+            # plt.imshow(label[1,:,:,1],cmap='gray')
+            # plt.show()
+
+
             logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
             net = unet.Unet(layers=4, features_root=16, channels=1, n_class=2 , summaries=True) #  , cost="dice_coefficient"
 
             trainer = unet.Trainer(net)
-            path = trainer.train(TrainData, Trained_Model_Path_Nuclei, training_iters=50, epochs=25, display_step=500) #, training_iters=100, epochs=100, display_step=10
+            path = trainer.train(TrainData, Directory_Nuclei_Train_Model, training_iters=100, epochs=100, display_step=500) #, training_iters=100, epochs=100, display_step=10
 
-            # tensorboard --logdir=~/artin/data/Thalamus/ForUnet_Test2_IncreasingNumSlices/TestSubject0/train/model
 
-            # OriginalSeg , Header , Affine = SumMasks(Directory_OriginalData+subFolders[sFi])
-            OriginalSegFull = nib.load(SegmentName)
-            Header = OriginalSegFull.header
-            Affine = OriginalSegFull.affine
-            OriginalSeg = OriginalSegFull.get_data()
+            OriginalSegFull = nib.load(Directory_Nuclei_Label)
+            # Header = OriginalSegFull.header
+            # Affine = OriginalSegFull.affine
+            # OriginalSeg = OriginalSegFull.get_data()
 
 
             CropDimensions = np.array([ [50,198] , [130,278] , [SliceNumbers[0] , SliceNumbers[len(SliceNumbers)-1]] ])
 
             padSize = 90
-
-            # [data,label,prediction] = TestData(net , Test_Path_Nuclei , Trained_Model_Path_Nuclei , OriginalSeg , Header , Affine , subFolders[sFi] , CropDimensions , padSize)
-        # #   TestData2_MultipliedByWholeThalamus(net , Test_Path_Nuclei , Trained_Model_Path_Nuclei , OriginalSeg , subFolders[sFi], CropDimensions , padSize , Test_Path_Thalamus , Trained_Model_Path_Thalamus):
-            # [data,label,prediction,OriginalSeg] = TestData2_MultipliedByWholeThalamus(net , Test_Path_Nuclei , Trained_Model_Path_VLP , OriginalSeg , subFolders[sFi] , CropDimensions , padSize , Test_Path_Thalamus , Trained_Model_Path_Thalamus , NucleusName)
+            [data,label,prediction] = TestData(net , Directory_Nuclei_Test , Directory_Nuclei_Train , OriginalSegFull , subFolders[sFi] , CropDimensions , padSize)
+            #   [data,label,prediction,OriginalSeg] = TestData2_MultipliedByWholeThalamus(net , Directory_Nuclei_Test , Directory_Nuclei_Train , OriginalSeg , subFolders[sFi] , CropDimensions , padSize , Directory_Thalamus_Test , Directory_Thalamus_TrainedModel , NucleusName)

e25273ee4dc924f79509e547877dcea60796148f
artin <msm2024@gmail.com>
2018-07-06 19:34:48 -0700

    b

--- SavingTiff_V6_BothMethod_forNewPriors.py
+++ SavingTiff_V6_BothMethod_forNewPriors.py
@@ -66,7 +66,7 @@ for ii in range(len(A)):
     if ii == 0:
         TestName = 'WMnMPRAGE_Deformed' # _Deformed_Cropped
     else:
-        TestName = 'WMnMPRAGE_Sharpness_' + str(A[ii][0]) + '_Contrast_' + str(A[ii][1]) + '_Deformed'
+        TestName = 'WMnMPRAGE_Deformed_Sharpness_' + str(A[ii][0]) + '_Contrast_' + str(A[ii][1]) 
 
     Dir_AllTests_Nuclei_EnhancedFld = Dir_AllTests + '/' + Name_allTests_Nuclei + '/Test_' + TestName
 

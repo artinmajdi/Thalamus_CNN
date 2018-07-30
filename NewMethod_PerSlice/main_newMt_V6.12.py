f73ace7bdeb8701fa66b7bca93ff1d019968ab4f
artin <msm2024@gmail.com>
2018-07-30 13:45:18 -0700

    f

--- NewMethod_PerSlice/main_newMt_V6.12.py
+++ NewMethod_PerSlice/main_newMt_V6.12.py
@@ -102,12 +102,13 @@ def initialDirectories(ind = 1, mode = 'local' , dataset = 'old' , method = 'new
     elif 'server' in mode:
 
         Params['modelFormat'] = 'cpkt'
+        hardDrive = 'ssd'
         if 'old' in dataset:
-            Dir_Prior = '/array/hdd/msmajdi/data/priors_forCNN_Ver2'
+            Dir_Prior = '/array/' + hardDrive + '/msmajdi/data/priors_forCNN_Ver2'
         elif 'new' in dataset:
-            Dir_Prior = '/array/hdd/msmajdi/data/newPriors/7T_MS'
+            Dir_Prior = '/array/' + hardDrive + '/msmajdi/data/newPriors/7T_MS'
 
-        Dir_AllTests  = '/array/hdd/msmajdi/Tests/Thalamus_CNN/' + dataset + 'Dataset_' + method +'Method' # 'oldDataset' #
+        Dir_AllTests  = '/array/' + hardDrive + '/msmajdi/Tests/Thalamus_CNN/' + dataset + 'Dataset_' + method +'Method' # 'oldDataset' #
 
 
 

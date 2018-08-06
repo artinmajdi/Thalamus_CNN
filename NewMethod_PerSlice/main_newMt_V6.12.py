b31ac5f91c5554a88f64363ff1789b93534b57c9
artinmajdi <msm2024@gmail.com>
2018-08-05 20:15:04 -0700

    s

--- NewMethod_PerSlice/main_newMt_V6.12.py
+++ NewMethod_PerSlice/main_newMt_V6.12.py
@@ -291,7 +291,7 @@ def paramIterEpoch(Params , slcIx):
         else:
             Params['epochs'] = 3 # 60
     else:
-        Params['epochs'] = 50
+        Params['epochs'] = 40
 
     return Params
 
@@ -347,7 +347,7 @@ for ind in UserEntries['IxNuclei']:
             # Params['training_iters'] = int(UserEntries['training_iters']) # 100
 
             dice = np.zeros(len(Params['SliceNumbers'])+1)
-            for slcIx in range(5,6): # ,len(Params['SliceNumbers'])):
+            for slcIx in range(5,6): # len(Params['SliceNumbers'])):
 
                 Params = paramIterEpoch(Params , slcIx)
                 print('epochs',Params['epochs'],'IxNuclei',Params['IxNuclei'],'iter',Params['training_iters'])

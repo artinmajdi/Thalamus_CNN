1429f430db2939f4fbbd70409416a052297b6705
artinmajdi <msm2024@gmail.com>
2018-08-19 00:12:51 -0700

    s

--- OldMethod/main_V6.131_adding_TerminalEnhanceAbility.py
+++ OldMethod/main_V6.131_adding_TerminalEnhanceAbility.py
@@ -17,6 +17,7 @@ from skimage import filters
 A = [[0,0],[6,1],[1,2],[1,3],[4,1]]
 
 
+
 def mkDir(dir):
     try:
         os.stat(dir)
@@ -218,7 +219,6 @@ for ind in UserEntries['IxNuclei']:
         subFolders = subFoldersFunc(Dir_AllTests_Nuclei_EnhancedFld)
 
         # subFolders = ['vimp2_ctrl_921_07122013_MP'] # vimp2_ctrl_920_07122013_SW'] #
-
         # aaa = range(14,len(subFolders))
         # aaa = np.append([0,1],aaa)
         for sFi in range(len(subFolders)):
@@ -273,3 +273,4 @@ for ind in UserEntries['IxNuclei']:
             info['net']          = Params['net']
 
             [Prediction3D_PureNuclei, Prediction3D_PureNuclei_logical] = TestData3_cleanedup(info , NucleiOrigSeg)
+

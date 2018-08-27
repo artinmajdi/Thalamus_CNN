773323550464c7a7abc15724a1757d933a798c44
artin <msm2024@gmail.com>
2018-08-26 21:01:16 -0700

    ds

--- OldMethod/ApplyingModelOnTest_V6.132.py
+++ OldMethod/ApplyingModelOnTest_V6.132.py
@@ -260,6 +260,14 @@ def trainFunc(Params):
 
     return path
 
+def copyPreviousModel(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 UserEntries = input_GPU_Ix()
 

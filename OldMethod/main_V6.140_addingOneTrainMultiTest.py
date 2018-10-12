87b182eebbdb90fee4cd8632876a3d16146ccfb7
artinmajdi <msm2024@gmail.com>
2018-10-11 17:07:10 -0700

    fsd

--- OldMethod/main_V6.140_addingOneTrainMultiTest.py
+++ OldMethod/main_V6.140_addingOneTrainMultiTest.py
@@ -337,24 +337,24 @@ for ind in UserEntries['IxNuclei']:
 
             trainer = unet.Trainer(Params['net'], optimizer = "adam")
 
-            if UserEntries['init']:
+            # if UserEntries['init']:
+            #
+            #     copyPreviousModel( Params['restorePath'], Dir_NucleiModelOut )
+            #     if Params['gpuNum'] != 'nan':
+            #         # path2 = ''
+            #         path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=400, epochs=150, display_step=500, GPU_Num=Params['gpuNum'] ,prediction_path=Dir_ResultsOut , restore='True')
+            #     else:
+            #         path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=10, epochs=3, display_step=500 ,prediction_path=Dir_ResultsOut , restore='True')
+            #
+            # else:
+            #     if Params['gpuNum'] != 'nan':
+            #         # path2 = ''
+            #         path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=400, epochs=150, display_step=500, GPU_Num=Params['gpuNum'] ,prediction_path=Dir_ResultsOut) #  restore=True
+            #     else:
+            #         path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=50, epochs=4, display_step=500 ,prediction_path=Dir_ResultsOut) #   restore=True
 
-                copyPreviousModel( Params['restorePath'], Dir_NucleiModelOut )
-                if Params['gpuNum'] != 'nan':
-                    # path2 = ''
-                    path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=400, epochs=150, display_step=500, GPU_Num=Params['gpuNum'] ,prediction_path=Dir_ResultsOut , restore='True')
-                else:
-                    path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=10, epochs=3, display_step=500 ,prediction_path=Dir_ResultsOut , restore='True')
 
-            else:
-                if Params['gpuNum'] != 'nan':
-                    # path2 = ''
-                    path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=400, epochs=150, display_step=500, GPU_Num=Params['gpuNum'] ,prediction_path=Dir_ResultsOut) #  restore=True
-                else:
-                    path = trainer.train(TrainData, Dir_NucleiModelOut, training_iters=50, epochs=4, display_step=500 ,prediction_path=Dir_ResultsOut) #   restore=True
-
-
-            if UserEntries['testmode'] == 'normal':
+            if 'Unlabeled' not in UserEntries['dataset']:
 
                 NucleiOrigSeg   = nib.load( Params['Dir_Prior'] + '/' + subFolders[sFi] + '/Manual_Delineation_Sanitized/' + Params['NucleusName'] + '_deformed.nii.gz' )
                 ThalamusOrigSeg = nib.load( Params['Dir_Prior'] + '/' + subFolders[sFi] + '/Manual_Delineation_Sanitized/' +        '1-THALAMUS'   + '_deformed.nii.gz' )

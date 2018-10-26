a567190f6b0797652f11cb7dcec30019a5c29f63
artinmajdi <msm2024@gmail.com>
2018-10-25 18:11:55 -0700

    bv

--- notMainCodes/test2.py
+++ notMainCodes/test2.py
@@ -1,193 +1,34 @@
 import matplotlib.pylab as plt
 import numpy as np
-import os
-import pickle
 import nibabel as nib
-import shutil
-from collections import OrderedDict
-import logging
-from tf_unet import unet, util, image_util
-import os
-import multiprocessing
-import matplotlib.pyplot as plt
 import tifffile
 
+im = nib.load('/media/data1/artin/thomas/origtemplate.nii.gz')
+# mask = nib.load('/media/data1/artin/code/Thalamus_CNN/notMainCodes/RigidRegistration/1-THALAMUS.nii.gz').get_data()
 
 
-def DiceCoefficientCalculator(msk1,msk2):
-    intersection = msk1*msk2  # np.logical_and(msk1,msk2)
-    DiceCoef = intersection.sum()*2/(msk1.sum()+msk2.sum() + np.finfo(float).eps)
-    return DiceCoef
-
-def mkDir(dir):
-    try:
-        os.stat(dir)
-    except:
-        os.makedirs(dir)
-    return dir
-
-def testNme(A,ii):
-    if ii == 0:
-        TestName = 'Test_WMnMPRAGE_bias_corr_Deformed'
-    else:
-        TestName = 'Test_WMnMPRAGE_bias_corr_Sharpness_' + str(A[ii][0]) + '_Contrast_' + str(A[ii][1]) + '_Deformed'
-        return TestName
-
-def initialDirectories(ind = 1, mode = 'local' , dataset = 'old' , method = 'old'):
-
-    Params = {}
-
-    if ind == 1:
-        NucleusName = '1-THALAMUS'
-        # SliceNumbers = range(106,143)
-        SliceNumbers = range(103,147)
-        # SliceNumbers = range(107,140) # original one
-    elif ind == 2:
-        NucleusName = '2-AV'
-        SliceNumbers = range(126,143)
-    elif ind == 4567:
-        NucleusName = '4567-VL'
-        SliceNumbers = range(114,143)
-    elif ind == 4:
-        NucleusName = '4-VA'
-        SliceNumbers = range(116,140)
-    elif ind == 5:
-        NucleusName = '5-VLa'
-        SliceNumbers = range(115,133)
-    elif ind == 6:
-        NucleusName = '6-VLP'
-        SliceNumbers = range(115,145)
-    elif ind == 7:
-        NucleusName = '7-VPL'
-        SliceNumbers = range(114,141)
-    elif ind == 8:
-        NucleusName = '8-Pul'
-        SliceNumbers = range(112,141)
-    elif ind == 9:
-        NucleusName = '9-LGN'
-        SliceNumbers = range(105,119)
-    elif ind == 10:
-        NucleusName = '10-MGN'
-        SliceNumbers = range(107,121)
-    elif ind == 11:
-        NucleusName = '11-CM'
-        SliceNumbers = range(115,131)
-    elif ind == 12:
-        NucleusName = '12-MD-Pf'
-        SliceNumbers = range(115,140)
-    elif ind == 13:
-        NucleusName = '13-Hb'
-        SliceNumbers = range(116,129)
-    elif ind == 14:
-        NucleusName = '14-MTT'
-        SliceNumbers = range(104,135)
+im.shape
+# mask.shape
 
 
-    Params['modelFormat'] = 'ckpt'
-    if 'localLT' in mode:
-
-        if '20priors' in dataset:
-            Dir_Prior = '/media/artin/dataLocal1/dataThalamus/20priors'
-        elif 'MS' in dataset:
-            Dir_Prior = '/media/artin/dataLocal1/dataThalamus/7T_MS'
-        elif 'ET_3T' in dataset:
-            Dir_Prior = '/media/artin/dataLocal1/dataThalamus/ET/3T'
-        elif 'ET_7T' in dataset:
-            Dir_Prior = '/media/artin/dataLocal1/dataThalamus/ET/7T'
-        elif 'Unlabeled' in dataset:
-            Dir_Prior = '/media/artin/dataLocal1/dataThalamus/Unlabeled'
-
-        Dir_AllTests  = '/media/artin/dataLocal1/dataThalamus/AllTests/' + dataset + 'Dataset_' + method +'Method'
-        if 'Unlabeled' in dataset:
-            Params['Dir_AllTests_restore']  = '/media/artin/dataLocal1/dataThalamus/AllTests/' + '20priors' + 'Dataset_' + 'old' +'Method'
-        else:
-            Params['Dir_AllTests_restore']  = '/media/artin/dataLocal1/dataThalamus/AllTests/' + 'Unlabeled' + 'Dataset_' + 'old' +'Method'
-
-
-    elif 'localPC' in mode:
-
-        if '20priors' in dataset:
-            Dir_Prior = '/media/data1/artin/thomas/priors/20priors'
-        elif 'MS' in dataset:
-            Dir_Prior = '/media/data1/artin/thomas/priors/7T_MS'
-        elif 'ET_3T' in dataset:
-            Dir_Prior = '/media/data1/artin/thomas/priors/ET/3T'
-        elif 'ET_7T' in dataset:
-            Dir_Prior = '/media/data1/artin/thomas/priors/ET/7T'
-        elif 'Unlabeled' in dataset:
-            Dir_Prior = '/media/data1/artin/thomas/priors/Unlabeled'
-
-        Dir_AllTests  = '/media/data1/artin/Tests/Thalamus_CNN/' + dataset + 'Dataset_' + method +'Method'
-
-        if 'Unlabeled' in dataset:
-            Params['Dir_AllTests_restore']  = '/media/data1/artin/Tests/Thalamus_CNN/' + '20priors' + 'Dataset_' + 'old' +'Method'
-        else:
-            Params['Dir_AllTests_restore']  = '/media/data1/artin/Tests/Thalamus_CNN/' + 'Unlabeled' + 'Dataset_' + 'old' +'Method'
-
-    elif 'server' in mode:
-
-        if '20priors' in dataset:
-            Dir_Prior = '/array/ssd/msmajdi/data/20priors'
-        elif 'MS' in dataset:
-            Dir_Prior = '/array/ssd/msmajdi/data/7T_MS'
-        elif 'ET_3T' in dataset:
-            Dir_Prior = '/array/ssd/msmajdi/data/ET/3T'
-        elif 'ET_7T' in dataset:
-            Dir_Prior = '/array/ssd/msmajdi/data/ET/7T'
-        elif 'Unlabeled' in dataset:
-            Dir_Prior = '/array/ssd/msmajdi/data/Unlabeled'
-
-        Dir_AllTests  = '/array/ssd/msmajdi/Tests/Thalamus_CNN/' + dataset + 'Dataset_' + method +'Method'
-
-        if 'Unlabeled' in dataset:
-            Params['Dir_AllTests_restore']  = '/array/ssd/msmajdi/Tests/Thalamus_CNN/' + '20priors' + 'Dataset_' + 'old' +'Method'
-        else:
-            Params['Dir_AllTests_restore']  = '/array/ssd/msmajdi/Tests/Thalamus_CNN/' + 'Unlabeled' + 'Dataset_' + 'old' +'Method'
-
-
-
-    Params = {}
-    Params['A'] = [[0,0],[6,1],[1,2],[1,3],[4,1]]
-    Params['Dir_Prior']    = Dir_Prior
-    Params['Dir_AllTests'] = Dir_AllTests
-    Params['SliceNumbers'] = SliceNumbers
-    Params['NucleusName']  = NucleusName
-
-    return Params
-
-
-dir = '/media/data1/artin/Tests/Thalamus_CNN/UnlabeledDataset_oldMethod/CNN1_THALAMUS_2D_SanitizedNN/Test_WMnMPRAGE_bias_corr/OneTrain_MultipleTest/Train'
-
-mask = tifffile.imread(dir + '/vimp2_2039_03182016_Sh0_Ct0_Slice_200_mask.tif')
-im = tifffile.imread(dir + '/vimp2_2039_03182016_Sh0_Ct0_Slice_200.tif')
-
-
+mask = np.zeros(im.shape)
 mask.shape
-plt.imshow(mask>0.2,cmap='gray')
+
+cp = [[72,146],[154,236],[85,162]]
+
+mask[cp[0][0]:cp[0][1] , cp[1][0]:cp[1][1] , cp[2][0]:cp[2][1] ] = 1
+
+output = nib.Nifti1Image(mask,im.affine)
+output.get_header = im.header
+nib.save(output , '/media/data1/artin/mask.nii.gz')
+
+
+fig , ax = plt.subplots(1,2)
+ax[0].imshow(im[:,200,:],cmap='gray')
+ax[1].imshow(mask[:,200,:],cmap='gray')
 plt.show()
 
-ind = 1
-mode = 'oldDatasetV2'
-NucleusName, NeucleusFolder, ThalamusFolder, Dir_AllTests, Dir_Prior = initialDirectories(ind = 1, mode = 'localPC' , dataset = '20priors' , method = 'old')
 
-dir = '/media/data1/artin/Test/'
-sub = os.listdir(dir)
-im = tifffile.imread(dir + sub[10])
-im.max()
-
-im = nib.load(dir + 'WMnMPRAGE_bias_corr_Deformed.nii.gz')
-im = im.get_data()
-
-plt.imshow(im[...,100],cmap='gray')
-plt.show()
-
-im2 = (im-im.mean())/im.std()
-
-im.max()
-tifffile.imsave( '/media/data1/artin/thomas/priors/20priors/vimp2_869_06142013_BL/ttt' , im2[...,100] )
-K = tifffile.imread( '/media/data1/artin/thomas/priors/20priors/vimp2_869_06142013_BL/ttt')
-
-K.min()
-im2[...,100].min()
-plt.imshow(K,cmap='gray')
-plt.show()
+# 3rd: 105 , 142
+# 2nd:  164 , 226
+# 1st 92 , 126

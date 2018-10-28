c40c3139b0f01dae3e3d508c305a32eeebde0c9b
artinmajdi <msm2024@gmail.com>
2018-10-28 14:25:58 -0700

    hgf

--- OldMethod/withoutRegistration/ApplyingModelOnTest_V6.142_forRigidCrop.py
+++ OldMethod/withoutRegistration/ApplyingModelOnTest_V6.142_forRigidCrop.py
@@ -464,7 +464,7 @@ def saveImageDice(label , Params , pred , pred_Lgc , subFolders):
     diceF[0] = DiceCoefficientCalculator(output_Lgc,labelF)
     np.savetxt(Params['Dir_ResultsOut'] + 'DiceCoefficientF.txt',diceF)
 
-savingThalamusPredOnTrainData = 0
+savingThalamusPredOnTrainData = 1
 UserEntries = input_GPU_Ix()
 
 for ind in UserEntries['IxNuclei']:
@@ -492,6 +492,7 @@ for ind in UserEntries['IxNuclei']:
 
             if (savingThalamusPredOnTrainData == 1) & ('1-THALAMUS' in Params['NucleusName']):
                 subFolders = subFoldersFunc( Params['Dir_Prior'] )
+                subFolders = subFolders[:5]
             else:
                 subFolders = subFoldersFunc(Dir_AllTests_Nuclei_EnhancedFld + 'OneTrain_MultipleTest' + '/TestCases/')
 

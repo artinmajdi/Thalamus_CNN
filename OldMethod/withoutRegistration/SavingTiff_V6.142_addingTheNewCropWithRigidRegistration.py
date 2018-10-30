c252967dc85a6190aa5b1cfd09185200ab240040
artin <msm2024@gmail.com>
2018-10-29 19:49:57 -0700

    gfd

--- OldMethod/withoutRegistration/SavingTiff_V6.142_addingTheNewCropWithRigidRegistration.py
+++ OldMethod/withoutRegistration/SavingTiff_V6.142_addingTheNewCropWithRigidRegistration.py
@@ -312,7 +312,7 @@ def funcCropping_FromThalamus(im , mask , CropMask):
     c3 = np.where(np.sum(ss,axis=0) > 1)[0]
 
     gap = 20
-    gap2 = 3
+    gap2 = 2
     d1 = [  c1[0]-gap  , c1[ c1.shape[0]-1 ]+gap   ]
     d2 = [  c2[0]-gap  , c2[ c2.shape[0]-1 ]+gap   ]
     SN = [  c3[0]-gap2 , c3[ c3.shape[0]-1 ]+gap2  ]

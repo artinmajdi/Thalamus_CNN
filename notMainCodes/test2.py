e160c5de2666afe8c8750dffc14060b47a4e954c
artinmajdi <msm2024@gmail.com>
2018-10-31 18:13:49 -0700

    jgh

--- notMainCodes/test2.py
+++ notMainCodes/test2.py
@@ -2,33 +2,21 @@ import matplotlib.pylab as plt
 import numpy as np
 import nibabel as nib
 import tifffile
+from scipy.misc import imrotate
 
 
-
-dir = '/media/data1/artin/thomas/origtemplate.nii.gz'
-im = nib.load(dir)
-
-mask = np.zeros(im.shape)
-
-gap = 20
-gap2 = 2
-mask[92-gap:126+gap,164-gap2:226+gap2, 105-gap:142+gap] = 1
-
-maskF2 = nib.Nifti1Image(mask,im.affine)
-maskF2.get_header = im.header
-nib.save(maskF2,'/media/data1/artin/code/Thalamus_CNN/notMainCodes/RigidRegistration/MyCrop_Template2_Gap20.nii.gz' )
-
+do_I_want_Upsampling = 0
 
 def funcNormalize(im):
     # return (im-im.mean())/im.std()
     im = np.float32(im)
     return ( im-im.min() )/( im.max() - im.min() )
 
-def cropDimensions(im , mask , CropMask):
-    ss = np.sum(CropMask,axis=2)
+def funcCropping(im , mask , CropMask2):
+    ss = np.sum(CropMask2,axis=2)
     c1 = np.where(np.sum(ss,axis=1) > 10)[0]
     c2 = np.where(np.sum(ss,axis=0) > 10)[0]
-    ss = np.sum(CropMask,axis=1)
+    ss = np.sum(CropMask2,axis=1)
     c3 = np.where(np.sum(ss,axis=0) > 10)[0]
 
     d1 = [  c1[0] , c1[ c1.shape[0]-1 ]  ]
@@ -41,29 +29,105 @@ def cropDimensions(im , mask , CropMask):
 
     return im , mask
 
-dir = '/media/data1/artin/vimp2_0699_04302014'
+def funcFlipLR_Upsampling(Params, im , mask):
+    if 'Unlabeled' in Params['dataset']:
 
-mask    = nib.load(dir + '/' + 'Manual_Delineation_Sanitized/' + '1-THALAMUS' + '.nii.gz').get_data()
-imF       = nib.load(dir + '/' + 'WMnMPRAGE_bias_corr.nii.gz' )
-CropMask = nib.load(dir + '/' + 'MyCrop.nii.gz').get_data()
+        for i in range(mask.shape[2]):
+            im[...,i] = np.fliplr(im[...,i])
+            mask[...,i] = np.fliplr(mask[...,i])
 
-Thalamus = nib.load('/media/data1/artin/Results/vimp2_1530_04232015_1-THALAMUS_Logical.nii.gz')
+        if do_I_want_Upsampling == 1:
+            mask = ndimage.zoom(mask,(1,1,2),order=0)
+            im = ndimage.zoom(im,(1,1,2),order=3)
+    else:
+        im   = np.transpose(im,[0,2,1])
+        mask = np.transpose(mask,[0,2,1])
 
-ss = np.sum(Thalamus.get_data(),axis=2)
-c1 = np.where(np.sum(ss,axis=1) > 1)[0]
-c2 = np.where(np.sum(ss,axis=0) > 1)[0]
-ss = np.sum(Thalamus.get_data(),axis=1)
-c3 = np.where(np.sum(ss,axis=0) > 1)[0]
+        if im.shape[2] == 200:
+            im = ndimage.zoom(im,(1,1,2),order=3)
+            mask = ndimage.zoom(mask,(1,1,2),order=0)
 
-d1 = [  c1[0] , c1[ c1.shape[0]-1 ]  ]
-d2 = [  c2[0] , c2[ c2.shape[0]-1 ]  ]
-SN = [  c3[0] , c3[ c3.shape[0]-1 ]  ]
-SliceNumbers = range(SN[0],SN[1])
+    return im , mask
 
-mask = np.zeros(CropMask.shape)
-gap = 5
-mask[ d1[0]-gap:d1[1]+gap,d2[0]-gap:d2[1]+gap,SliceNumbers] = 1
+def funcPadding(im, mask):
+    sz = mask.shape
+    df = 238 - sz[0]
+    p1 = [int(df/2) , df - int(df/2)]
 
-maskF2 = nib.Nifti1Image(mask,Thalamus.affine)
-maskF2.get_header = Thalamus.header
-nib.save(maskF2,'/media/data1/artin/Results/mask_from_Thalamus.nii.gz' )
+    df = 238 - sz[1]
+    p2 = [int(df/2) , df - int(df/2)]
+
+    im = np.pad(im,( (p1[0],p1[1]),(p2[0],p2[1]),(0,0) ),'constant' )
+    mask = np.pad(mask,( (p1[0],p1[1]),(p2[0],p2[1]),(0,0) ),'constant' )
+
+    return im , mask
+
+def funcRotating(im, mask, CropMask):
+
+    angle = np.random.random_integers(45)
+
+    for i in range(im.shape[2]):
+        im[...,i] = imrotate(im[...,i],angle)
+        mask[...,i] = imrotate(mask[...,i],angle)
+        CropMask[...,i] = imrotate(CropMask[...,i],angle)
+
+    return im, mask, CropMask
+
+def funcShifting(im, mask):
+
+    shftX = np.random.random_integers(15)
+    shftY = np.random.random_integers(15)
+
+    im = np.roll(im,shftX,axis=0)
+    im = np.roll(im,shftY,axis=1)
+
+    mask = np.roll(mask,shftX,axis=0)
+    mask = np.roll(mask,shftY,axis=1)
+
+    return im, mask
+
+
+dir = '/media/data1/artin/vimp2_1621_10162015'
+Params = {'dataset':'Unlabeled'}
+
+im2   = nib.load(dir + '/' + 'WMnMPRAGE_bias_corr.nii.gz' ).get_data()
+mask2 = nib.load(dir + '/' + 'Manual_Delineation_Sanitized/' + '1-THALAMUS' + '.nii.gz').get_data()
+CropMask2 = nib.load(dir + '/' + 'MyCropMask2_Gap20.nii.gz' ).get_data()
+
+im2 = funcNormalize(im2)
+
+im = im2.copy()
+mask = mask2.copy()
+CropMask = CropMask2.copy()
+
+im2, mask2 = funcCropping(im2, mask2, CropMask2)
+im2, mask2 = funcFlipLR_Upsampling(Params, im2 , mask2)
+imP2, maskP2 = funcPadding(im2, mask2)
+
+im, mask, CropMask = funcRotating(im, mask, CropMask)
+im, mask = funcCropping(im, mask, CropMask)
+im, mask = funcFlipLR_Upsampling(Params, im , mask)
+imP, maskP = funcPadding(im, mask)
+imP, maskP = funcShifting(imP, maskP)
+
+slc = 20
+fig, axs = plt.subplots(2,2)
+axs[0,0].imshow(imP2[...,slc],cmap='gray')
+axs[0,1].imshow(imP[...,slc],cmap='gray')
+axs[1,0].imshow(maskP2[...,slc],cmap='gray')
+axs[1,1].imshow(maskP[...,slc],cmap='gray')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+# maskF2 = nib.Nifti1Image(mask,Thalamus.affine)
+# maskF2.get_header = Thalamus.header
+# nib.save(maskF2,'/media/data1/artin/Results/mask_from_Thalamus.nii.gz' )
